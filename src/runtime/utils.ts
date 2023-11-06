5477f3801857e789e3e16d015043bb114b659eff
kiki-kanri <a470666@gmail.com>
2023-11-07 04:27:47 +0800

    feat: add get unique storage key utils

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,11 +1,20 @@
+import { uid } from 'uid';
 import { createStorage } from 'unstorage';
 import fsDriver from 'unstorage/drivers/fs';
 import fsLiteDriver from 'unstorage/drivers/fs-lite';
 import lruCacheDriver from 'unstorage/drivers/lru-cache';
 import memoryDriver from 'unstorage/drivers/memory';
 import redisDriver from 'unstorage/drivers/redis';
+import type { Storage, StorageValue } from 'unstorage';
 
 import type { UseStorageModuleOptions } from '../types';
+
+export const generateUniqueSessionStorageKey = async (storageOptions: Required<Required<UseStorageModuleOptions>['storage']>, storage: Storage<StorageValue>) => {
+	let key: string;
+	do key = `${storageOptions.keyPrefix}_${uid(storageOptions.keyLength)}`;
+	while (await storage.hasItem(key));
+	return key;
+};
 
 export const getStorage = (moduleOptions: UseStorageModuleOptions) => {
 	if (!moduleOptions.storage || moduleOptions.storage?.driver === 'memory') return createStorage({ driver: memoryDriver() });
