cddf9d55a47e201211f931bfbbd99152f0200a32
kiki-kanri <a470666@gmail.com>
2023-11-08 07:28:16 +0800

    feat: add session data encryption and decryption functions

--- src/runtime/utils.ts
+++ src/runtime/utils.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto';
 import { nanoid } from 'nanoid';
 import { createStorage } from 'unstorage';
 import fsDriver from 'unstorage/drivers/fs';
@@ -7,7 +8,36 @@ import memoryDriver from 'unstorage/drivers/memory';
 import redisDriver from 'unstorage/drivers/redis';
 import type { Storage, StorageValue } from 'unstorage';
 
-import type { RequiredModuleOptions } from '../types';
+import type { PartialH3EventContextSession, RequiredModuleOptions } from '../types';
+
+export const createSessionCipherFunctions = (secretKey: string) => {
+	if (secretKey.length !== 32) throw new Error('The secret length must be 32!');
+	const algorithm = 'aes-256-cbc';
+	const key = Buffer.from(secretKey, 'utf-8');
+	const ivLength = 16;
+	const decryptSession = (encryptedSession: string): PartialH3EventContextSession | undefined => {
+		const textParts = encryptedSession.split(':');
+		if (textParts.length === 1) return;
+		try {
+			const iv = Buffer.from(textParts.shift()!, 'hex');
+			const decipher = crypto.createDecipheriv(algorithm, key, iv);
+			return JSON.parse(`${decipher.update(textParts.join(':'), 'hex', 'utf8')}${decipher.final('utf8')}`);
+		} catch (error) {}
+	};
+
+	const encryptSession = (sessionData: PartialH3EventContextSession) => {
+		const iv = crypto.randomBytes(ivLength);
+		const cipher = crypto.createCipheriv(algorithm, key, iv);
+		try {
+			return `${iv.toString('hex')}:${cipher.update(JSON.stringify(sessionData), 'utf8', 'hex')}${cipher.final('hex')}`;
+		} catch (error) {}
+	};
+
+	return {
+		decryptSession,
+		encryptSession
+	};
+};
 
 export const generateUniqueSessionStorageKey = async (storageOptions: RequiredModuleOptions.UseUnstorage['storage'], storage: Storage<StorageValue>) => {
 	let key: string;
