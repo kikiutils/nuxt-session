c1df8947761521179ce4781df1f029e5e06e00dc
kiki-kanri <a470666@gmail.com>
2024-07-19 20:51:52 +0800

    feat: add main module file

---
+++ src/module.ts
@@ -0,0 +1,20 @@
+import { initialization } from '@kikiutils/nitro-session';
+import type { PluginOptions } from '@kikiutils/nitro-session/types/options';
+import { addServerImportsDir, addServerPlugin, addServerScanDir, createResolver, defineNuxtModule, useLogger } from '@nuxt/kit';
+
+export type {} from './types/h3';
+export type { H3EventContextSession } from './types/session';
+
+export default defineNuxtModule<PluginOptions>({
+	meta: { configKey: 'nuxtSession', name: '@kikiutils/nuxt-session' },
+	async setup(options, nuxt) {
+		const logger = useLogger();
+		const initializationResult = await initialization('Nuxt', options);
+		if (!initializationResult) return;
+		nuxt.options.runtimeConfig.nuxtSession = initializationResult.pluginOptions;
+		const resolver = createResolver(import.meta.url);
+		addServerImportsDir(resolver.resolve('./runtime/server/utils'));
+		addServerPlugin(resolver.resolve('./runtime/server/plugins/session'));
+		logger.success('Nuxt session initialization successful.');
+	}
+});
