1bb833b280758bf812507803e14afe996289f1e3
kiki-kanri <a470666@gmail.com>
2024-07-19 21:40:35 +0800

    feat: add plugin and utils file for runtime

---
+++ src/runtime/server/plugins/session.ts
@@ -0,0 +1,7 @@
+import { registerHooksAndSetupCachedHandlers } from '@kikiutils/nitro-session';
+import type { PluginOptions } from '@kikiutils/nitro-session/types/options';
+import type { NitroApp } from 'nitropack';
+
+import { useRuntimeConfig } from '#imports';
+
+export default (nitroApp: NitroApp) => registerHooksAndSetupCachedHandlers(nitroApp, useRuntimeConfig().nuxtSession as Required<PluginOptions>, true);
---
+++ src/runtime/server/utils/session.ts
@@ -0,0 +1,1 @@
+export { clearH3EventContextSession, deleteH3EventContextSessionStorageData, getH3EventContextSessionToken, popH3EventContextSession } from '@kikiutils/nitro-session/runtime/utils';
